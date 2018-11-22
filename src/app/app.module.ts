5dad3495aa34253231204757bf2777b387a28444
pmerencillo <MERENCILLOPAOLO@protonmail.ch>
2018-11-22 13:23:34 +0800

    added 2pages & 1 service for typical practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstpageComponent } from './firstpage/firstpage.component';
+import { SecondpageComponent } from './secondpage/secondpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstpageComponent,
+    SecondpageComponent
   ],
   imports: [
     BrowserModule,
